48a2badb0491f55f076843814a36d956f67f26ae
liu <842130398@qq.com>
2020-06-02 00:31:56 +0800

    add padding

--- p9/src/main/java/net/liu6/p9/MaterialEditText.kt
+++ p9/src/main/java/net/liu6/p9/MaterialEditText.kt
@@ -2,15 +2,25 @@ package net.liu6.p9
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Rect
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 import com.alpha.utils.dp2px
+
+
 
 class MaterialEditText(context: Context?, attrs: AttributeSet?) : AppCompatEditText(context, attrs) {
 
     val titleTextSize = dp2px(16)
     val titleMargin = dp2px(16)
 
+    var backgroundPadding = Rect()
+
+
+    init {
+        background.getPadding(backgroundPadding)
+        setPadding(paddingLeft, (backgroundPadding.top + titleTextSize + titleMargin).toInt(), paddingRight, paddingBottom)
+    }
 
 
 
